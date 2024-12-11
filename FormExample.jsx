768e7db56747911c52ca865c2971379b98f212b3
narasimha-254 <parampogunarasimhudu24@gmail.com>
2024-12-11 11:43:03 +0530

    added react-toastify using npm website

--- FormExample.jsx
+++ FormExample.jsx
@@ -1,19 +1,25 @@
 import React, { useState } from 'react'
 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 const FormExample = () => {
     const [username,setUsername]=useState("")
     const [newname,setNewname]=useState('')
 
+    const notify = () => toast(" Your data is Uploaded Successfully ");
     const changeValue =(e)=>{
         setUsername(e.target.value)
     }
     const submitValue = (e)=>{
         e.preventDefault();
         setNewname(username)
+        notify();
     }
   return (
     <div className='formSection'>
         <h1>Hello, {newname}</h1>
+        <ToastContainer />
         {/* Normal method */}
         {/* <input type='text' className='inputBox' placeholder='Enter Your Name' onChange={changeValue}/>
         <br />
